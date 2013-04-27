dca13b016c1f93108f7d972c51b78127b7038003
moodpo <yangxiaoxiehaha@gmail.com>
2013-04-27 10:03:36 +0800

    Update 404.php
    修改404页面图片引用错误。

--- 404.php
+++ 404.php
@@ -1,10 +1,10 @@
 <?php $this->need('header.php');?>
 <div class="container">
 	<div id="content" class="content">
-		<img style="display:block;margin:0 auto" src="img/404.gif" />
+		<img style="display:block;margin:0 auto" src="<?php $this->options->themeUrl('img/404.gif'); ?>" />
 		<p style="margin:0 auto;text-align:center; width: 70%;">
 			啊哦！这个页面貌似被无节操的外星人带走了呢！你可以<a href="javascript:history.go(-1)">返回上一页</a>。
-			如果是博主把你带到这里的话，请<a href="/guestbook">报告错误页面</a>。
+			如果是博主把你带到这里的话，请<a href="mailto:yourname@site.com">报告错误页面</a>。
 		</p>
 	</div>
 	<!-- siderbar -->
